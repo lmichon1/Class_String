d5de234d3e1f6a88c364e0865026b738d3061cb7
TRANtuanlinh <linhbeo_beo@yahoo.com>
2014-12-12 12:12:16 +0100

    new version

--- teststring.cpp
+++ teststring.cpp
@@ -1,10 +1,17 @@
 #include "cstring.h"
-#include<cstdlib>
-#include<cstdio>
+#include <cstdlib>
+#include <cstdio>
 
 int main(int argc, char* argv[])
 {
-  cstring  phrase/*=new cstring()*/;
-  printf("la taille vaut %d et la capacité vaut %d\n", phrase.getLength(),phrase.getCapacity());
-  
+
+  	cstring str1=cstring("toto1");
+  	cstring str3=str1;
+ 	
+
+  	
+	str1.showstring();
+	(str3+'o').showstring();
+	str3.showstring();
+  	printf("la taille vaut %d et la capacité vaut %d\n", str3.getLength(),str3.getCapacity());
 }
