ca245f981818d5808bb805a92cc07c25001b8e18
lmichon1 <lucas.michon1@insa-lyon.fr>
2014-12-16 15:43:02 +0100

    make main empty

--- teststring.cpp
+++ teststring.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char* argv[])
 {
-  cstring  phrase/*=new cstring()*/;
-  printf("la taille vaut %d et la capacité vaut %d\n", phrase.getLength(),phrase.getCapacity());
-  
-}
+
+  return 0;
+
+} 
